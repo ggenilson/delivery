cadcbe0f322d4d77d82503ed5efbfa8108853533
ggenilson <genilsonaraujo20002002@gmail.com>
2022-06-07 02:39:32 +0100

    feat: authenticate client

---
+++ src/modules/account/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,34 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { prisma } from '../../../database/prismaClient';
+
+interface IAuthenticateClient {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateClientUseCase {
+  async execute({ username, password }: IAuthenticateClient) {
+    const client = await prisma.clients.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!client) {
+      throw new Error('Username or password invalid');
+    }
+
+    const passwordMatch = await compare(password, client.password);
+
+    if (!passwordMatch) {
+      throw new Error('User or password invalid');
+    }
+    const token = sign({ username }, '49fb96f0ed1a4b5a3a80763d4ff762da', {
+      subject: client.id,
+      expiresIn: '1d',
+    });
+
+    return token;
+  }
+}
