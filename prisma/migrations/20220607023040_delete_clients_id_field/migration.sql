3edb3d67b39b675dfa1f2d4187f76848984335ab
ggenilson <genilsonaraujo20002002@gmail.com>
2022-06-07 03:43:48 +0100

    feat: save delivery

---
+++ prisma/migrations/20220607023040_delete_clients_id_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `clientsId` on the `deliveries` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "deliveries" DROP COLUMN "clientsId";
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/createClientController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/createDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/createDeliverymanController';
 
 const routes = Router();
@@ -11,6 +12,7 @@ const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post('/client/authenticate', authenticateClientController.handle);
 routes.post(
@@ -21,4 +23,5 @@ routes.post(
 routes.post('/client/', createClientController.handle);
 routes.post('/client/', createDeliverymanController.handle);
 
+routes.post('/delivery', createDeliveryController.handle);
 export { routes };
